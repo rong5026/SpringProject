e6cabb5e4dec3ad8b09d7a0022cb4537c38b4185
rong5026 <rong5026@naver.com>
2023-08-26 11:42:26 +0900

    [ADD] DIP위반 리펙토링

--- basic/src/main/java/spring/basic/order/OrderServiceImpl.java
+++ basic/src/main/java/spring/basic/order/OrderServiceImpl.java
@@ -2,14 +2,21 @@ package spring.basic.order;
 
 import spring.basic.discount.DiscountPolicy;
 import spring.basic.discount.FixDiscountPolicy;
+import spring.basic.discount.RateDiscountPolicy;
 import spring.basic.member.Member;
 import spring.basic.member.MemberRepository;
 import spring.basic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
--- basic/src/test/java/spring/basic/order/OrderServiceTest.java
+++ basic/src/test/java/spring/basic/order/OrderServiceTest.java
@@ -1,16 +1,24 @@
 package spring.basic.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.basic.AppConfig;
 import spring.basic.member.Grade;
 import spring.basic.member.Member;
 import spring.basic.member.MemberService;
 import spring.basic.member.MemberServiceImpl;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
 
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     @Test
     void createOrder() {
         long memberId = 1L;
