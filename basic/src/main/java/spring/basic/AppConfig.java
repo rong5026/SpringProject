8e5c02afb9c771bf20f12a95461aaa60f6760bda
rong5026 <rong5026@naver.com>
2023-08-29 09:39:42 +0900

    스프링으로 전환하기

--- basic/src/main/java/spring/basic/AppConfig.java
+++ basic/src/main/java/spring/basic/AppConfig.java
@@ -1,7 +1,10 @@
 package spring.basic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.basic.discount.DiscountPolicy;
 import spring.basic.discount.FixDiscountPolicy;
+import spring.basic.discount.RateDiscountPolicy;
 import spring.basic.member.MemberRepository;
 import spring.basic.member.MemberService;
 import spring.basic.member.MemberServiceImpl;
@@ -9,25 +12,30 @@ import spring.basic.member.MemoryMemberRepository;
 import spring.basic.order.OrderService;
 import spring.basic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
         );
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- basic/src/main/java/spring/basic/OrderApp.java
+++ basic/src/main/java/spring/basic/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.basic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.basic.member.Grade;
 import spring.basic.member.Member;
 import spring.basic.member.MemberService;
@@ -12,9 +14,14 @@ public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
