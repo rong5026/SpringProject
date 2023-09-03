0dbce7a21fc22056aba8211cbf9536d2c0d72703
rong5026 <rong5026@naver.com>
2023-09-03 23:34:30 +0900

    memberRepository의 싱글톤 확인 테스트

---
+++ basic/src/test/java/spring/basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package spring.basic.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.basic.AppConfig;
+import spring.basic.member.MemberRepository;
+import spring.basic.member.MemberServiceImpl;
+import spring.basic.order.OrderServiceImpl;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        //3개 모두 같은 인스턴스를 참고하고 있다.
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository  = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
